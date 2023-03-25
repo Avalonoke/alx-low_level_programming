6f2fc258a19df9f32ce1eebf7d9667e7c49fb229
Avalonoke <avalonoke35@gmail.com>
2023-03-24 21:59:25 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints the number from 1 to 100, followed by a new line
+ * but for multiples of three, print Fizz instead of the number
+ * and for multiples of five, print Buzz
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 |= 0)
